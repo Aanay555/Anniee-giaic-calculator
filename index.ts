95e99b2b0778d04840d909ac7b357b35bcc5168f
Aanay555 <160274256+Aanay555@users.noreply.github.com>
2024-04-05 18:04:56 +0500

    updated my Poject

--- index.ts
+++ index.ts
@@ -1,6 +1,35 @@
 #! /usr/bin/env node
 
     import inquirer from "inquirer";
+    import chalk from "chalk";
+    import chalkAnimation from "chalk-animation";
+//function start
+    function WellcomeCalculator() 
+    {
+      let  endanimation = chalkAnimation.karaoke(`
+         _____________________  
+        |  _________________  |
+        | | JO           0. | | 
+        | |_________________| |
+        |  ___ ___ ___   ___  |
+        | | 7 | 8 | 9 | | + | |
+        | |___|___|___| |___| |
+        | | 4 | 5 | 6 | | - | |
+        | |___|___|___| |___| |
+        | | 1 | 2 | 3 | | x | |
+        | |___|___|___| |___| |
+        | | . | 0 | = | | / | |
+        | |___|___|___| |___| |
+        |_____________________|
+
+        |u|z|m|a| |r|a|s|h|i|d|
+
+      `);
+        setTimeout(() => {
+          endanimation.stop();
+         }, 3000 ) ;  
+    }   
+  
     const answer = await inquirer.prompt([{message : "Enter 1st Number " , type : "number", name : "FirstNumber" },
     {
     message : "Enter your  2nd Number " , type : "number", name : "SecondNumber" },
@@ -8,26 +37,32 @@
     message : "Select on operator for performing action",
     type : "list",
     name : "Operator",
-    choices : ["Addition", "Subtraction", "Multiplication", "Division" , "%", "="],
+    choices : ["Addition", "Subtraction", "Multiplication", "Division" , "Power", "%", "="],
     },
     ]);
+   
 
     if(answer.Operator === "Addition"){
-        console.log(answer.FirstNumber + answer.SecondNumber);
+        console.log(chalk.bgGreen(answer.FirstNumber + answer.SecondNumber));
         
         } else if (answer.Operator === "Subtraction"){
-        console.log(answer.FirstNumber - answer.SecondNumber);
+        console.log(chalk.bgBlue(answer.FirstNumber - answer.SecondNumber));
         } else if (answer.Operator === "Multiplication"){
-        console.log(answer.FirstNumber * answer.SecondNumber);
+        console.log(chalk.bgBlueBright(answer.FirstNumber * answer.SecondNumber));
         } else if (answer.Operator === "Division"){
-        console.log(answer.FirstNumber / answer.SecondNumber);
+        console.log(chalk.bgBlueBright(answer.FirstNumber / answer.SecondNumber));
         }  else if(answer.Operator === "%"){
-        console.log(answer.FirstNumber * answer.SecondNumber / 100 ,"Rs."); 
-        }
-            else {
-          console.log("Select Valid Operator");
-        }
-        console.log("End of Programe");
-        
+        console.log(chalk.bgBlueBright(answer.FirstNumber * answer.SecondNumber / 100 ,"Rs.")); 
+        }else  if(answer.Operator === "Power"){
+          console.log(chalk.bgGreen(answer.FirstNumber ** answer.SecondNumber));
+  
+          }
 
+            else {
+          console.log(chalk.bgYellowBright("Select Valid Operator"));
+        }
+      
+              WellcomeCalculator();
+    //function end
+   
       
